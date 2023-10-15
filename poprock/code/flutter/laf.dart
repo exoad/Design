3824e102303e97867414dfc0358ef0a0ed2995d5
Jack Meng <45053009+exoad@users.noreply.github.com>
2023-10-15 12:20:38 -0400

    Update laf.dart

--- poprock/code/flutter/laf.dart
+++ poprock/code/flutter/laf.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PoprockLaF {
-  final Color primary1 = const Color.fromARGB(255, 255, 38, 103);
-  final Color primary2 = const Color.fromARGB(255, 23, 255, 108);
-  final Color primary3 = const Color.fromARGB(255, 250, 185, 22);
-  final Color bg = Colors.black;
+  static const Color primary1 = Color.fromARGB(255, 255, 38, 103);
+  static const Color primary2 = Color.fromARGB(255, 23, 255, 108);
+  static const Color primary3 = Color.fromARGB(255, 250, 185, 22);
+  static const Color bg = Colors.black;
 }
