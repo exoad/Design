d9ba9185f13a23e85a8813849499986e7a74bdf4
Jack Meng <45053009+exoad@users.noreply.github.com>
2023-10-15 12:18:36 -0400

    Create laf.dart

---
+++ poprock/code/flutter/laf.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PoprockLaF {
+  final Color primary1 = const Color.fromARGB(255, 255, 38, 103);
+  final Color primary2 = const Color.fromARGB(255, 23, 255, 108);
+  final Color primary3 = const Color.fromARGB(255, 250, 185, 22);
+  final Color bg = Colors.black;
+}
