c25ed2be43b143b15a6d8c5c82271a01c7d9ce81
Jack Meng <45053009+exoad@users.noreply.github.com>
2023-10-21 21:13:35 -0400

    `add` code for byakko theme

---
+++ byakko_(whitefox)/code/flutter/laf.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ByakkoLaF {
+  static const Color primary1 = Color.fromARGB(255, 47, 184, 254);
+  static const Color primary2 = Color.fromARGB(255, 250, 82, 134);
+  static const Color bg = Colors.white;
+}
